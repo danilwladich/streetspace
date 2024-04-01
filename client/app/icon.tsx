d6a1d7f1f613428ba3016a7298bd8be8120fcc93
Daniel <118133808+danilwladich@users.noreply.github.com>
2024-04-01 12:59:52 +0200

    client init

---
+++ client/app/icon.tsx
@@ -0,0 +1,42 @@
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+
+export const size = {
+	width: 32,
+	height: 32,
+};
+export const contentType = "image/png";
+
+export default function Icon() {
+	return new ImageResponse(
+		(
+			<div
+				style={{
+					width: "100%",
+					height: "100%",
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+				}}
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="white"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<path d="M6 2 3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4Z" />
+					<path d="M3 6h18" />
+					<path d="M16 10a4 4 0 0 1-8 0" />
+				</svg>
+			</div>
+		),
+		{ ...size }
+	);
+}
---
+++ client/app/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { AuthProvider } from "@/components/providers/auth-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { AlertDialogProvider } from "@/components/providers/alert-dialog-provider";
+import { Toaster } from "@/components/ui/sonner";
+import { getAppTitle } from "@/lib/get-app-title";
+
+const font = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: getAppTitle(),
+	description: "2rnik union",
+	authors: [
+		{ name: "Daniel Władyczewski", url: "https://github.com/danilwladich" },
+	],
+	icons: [
+		{
+			rel: "icon",
+			url: "/icon?<generated>",
+			sizes: "32x32",
+			type: "image/png",
+		},
+	],
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body className={font.className}>
+				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+					<AuthProvider>
+						<ModalProvider />
+						<AlertDialogProvider />
+						{children}
+						<Toaster />
+					</AuthProvider>
+				</ThemeProvider>
+			</body>
+		</html>
+	);
+}
