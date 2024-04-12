4f0f5d553fdbacb95e786efd6a199e84b19b9fa1
Daniel <118133808+danilwladich@users.noreply.github.com>
2024-04-12 20:12:15 +0200

    Map metadata

--- client/app/(main)/(routes)/map/page.tsx
+++ client/app/(main)/(routes)/map/page.tsx
@@ -1,7 +1,13 @@
+import type { Metadata } from "next";
+import { getAppTitle } from "@/lib/get-app-title";
 import dynamic from "next/dynamic";
 import { useMemo } from "react";
 
 import { AppLoader } from "@/components/ui/app-loader";
+
+export const metadata: Metadata = {
+  title: getAppTitle('map')
+};
 
 export default function Map() {
   const MapContainer = useMemo(
