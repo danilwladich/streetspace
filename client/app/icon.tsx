1f0a3e9df9cd82fd9a7b82717c1549103368db08
Daniel <118133808+danilwladich@users.noreply.github.com>
2024-04-12 20:27:39 +0200

    Update icon component

--- client/app/icon.tsx
+++ client/app/icon.tsx
@@ -3,40 +3,42 @@ import { ImageResponse } from "next/og";
 export const runtime = "edge";
 
 export const size = {
-	width: 32,
-	height: 32,
+  width: 32,
+  height: 32,
 };
 export const contentType = "image/png";
 
 export default function Icon() {
-	return new ImageResponse(
-		(
-			<div
-				style={{
-					width: "100%",
-					height: "100%",
-					display: "flex",
-					alignItems: "center",
-					justifyContent: "center",
-				}}
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="none"
-					stroke="white"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-				>
-					<path d="M6 2 3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4Z" />
-					<path d="M3 6h18" />
-					<path d="M16 10a4 4 0 0 1-8 0" />
-				</svg>
-			</div>
-		),
-		{ ...size }
-	);
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <svg
+          data-v-f6279e27=""
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#fff"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-dumbbell lucide-icon"
+        >
+          <path d="M14.4 14.4 9.6 9.6"></path>
+          <path d="M18.657 21.485a2 2 0 1 1-2.829-2.828l-1.767 1.768a2 2 0 1 1-2.829-2.829l6.364-6.364a2 2 0 1 1 2.829 2.829l-1.768 1.767a2 2 0 1 1 2.828 2.829z"></path>
+          <path d="m21.5 21.5-1.4-1.4"></path>
+          <path d="M3.9 3.9 2.5 2.5"></path>
+          <path d="M6.404 12.768a2 2 0 1 1-2.829-2.829l1.768-1.767a2 2 0 1 1-2.828-2.829l2.828-2.828a2 2 0 1 1 2.829 2.828l1.767-1.768a2 2 0 1 1 2.829 2.829z"></path>
+        </svg>
+      </div>
+    ),
+    { ...size },
+  );
 }
