62b8d52629b67b5ec365feecc85ea6ba7c905d3a
Daniel <118133808+danilwladich@users.noreply.github.com>
2024-04-13 13:18:51 +0200

    Remove unnecessary content from Home component

--- client/app/(main)/(routes)/page.tsx
+++ client/app/(main)/(routes)/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-	return <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea architecto facilis asperiores, perferendis vitae corrupti inventore beatae esse illo, velit fuga sed voluptas dolorem temporibus? Veritatis eligendi debitis maxime quo!
-	Cum, iure tempora minus itaque nihil officia nostrum minima quod? Rem possimus veritatis quibusdam, laboriosam quis alias natus, odio aspernatur, fuga laudantium eum velit neque sed molestiae blanditiis odit? Suscipit.
-	Magni dolorem, voluptates nihil at enim eaque nesciunt soluta accusamus vel, nobis reiciendis aspernatur laborum in animi assumenda? Facere commodi velit odio quisquam ea, quibusdam laborum non. Omnis, dolorem sit.
-	Voluptatem cum aliquid libero atque quia ipsum minus. Quisquam saepe provident nihil odit omnis! Quod iste quia quidem ratione quibusdam voluptas illum eos enim vero accusantium, alias corrupti rem ut.
-	Atque explicabo est vero dolorem odio quasi provident pariatur beatae harum, consequatur in voluptatem quidem eos quo laboriosam. Molestiae odio distinctio ut tempore aut explicabo architecto ducimus dolorum repellendus officiis!</div>;
+  return <div></div>;
 }
