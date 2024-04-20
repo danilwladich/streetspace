5b8e46e27fa86daf9801467d572fa1bd87e8f013
Daniel <118133808+danilwladich@users.noreply.github.com>
2024-04-20 17:11:55 +0200

    Fix useMapStore

--- hooks/store/use-map-store.ts
+++ hooks/store/use-map-store.ts
@@ -13,25 +13,35 @@ export interface Bounds {
 interface MapStore {
   loadingUserPosition: boolean;
   loadingMarkers: boolean;
+  position: LatLng | null;
+  zoom: number | null;
   userPosition: LatLng | null;
   bounds: Bounds | null;
   setLoadingUserPosition: (loadingUserPosition: boolean) => void;
   setLoadingMarkers: (loadingMarkers: boolean) => void;
+  setPosition: (position: LatLng) => void;
+  setZoom: (zoom: number) => void;
   setUserPosition: (userPosition: LatLng | null) => void;
   setBounds: (bounds: Bounds) => void;
 }
 
-const initialValues: { userPosition?: LatLng; bounds?: Bounds } = JSON.parse(
-  localStorage.getItem("mapData") || "{}",
-);
+const initialValues: {
+  position?: LatLng;
+  zoom?: number;
+  bounds?: Bounds;
+} = JSON.parse(localStorage.getItem("mapData") || "{}");
 
 export const useMapStore = create<MapStore>((set) => ({
   loadingUserPosition: true,
   loadingMarkers: true,
-  userPosition: initialValues.userPosition || null,
+  position: initialValues.position || null,
+  zoom: initialValues.zoom || null,
+  userPosition: null,
   bounds: initialValues.bounds || null,
   setLoadingUserPosition: (loadingUserPosition) => set({ loadingUserPosition }),
   setLoadingMarkers: (loadingMarkers) => set({ loadingMarkers }),
+  setPosition: (position) => set({ position }),
+  setZoom: (zoom) => set({ zoom }),
   setUserPosition: (userPosition) => set({ userPosition }),
   setBounds: (bounds) => set({ bounds }),
 }));
