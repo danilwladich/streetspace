5aefcec100a030b755401845204208c28ff255ac
Daniel <118133808+danilwladich@users.noreply.github.com>
2024-05-05 15:43:22 +0200

    Update Recaptcha component to use environment variable for site key

--- components/common/forms/recaptcha.tsx
+++ components/common/forms/recaptcha.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 
+const SITE_KEY = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || "sitekey";
+
 const Recaptcha = React.forwardRef<ReCAPTCHA>(({}, ref) => {
   return (
     <ReCAPTCHA
       className="fixed bottom-10 right-0 translate-x-0 translate-y-0 md:bottom-0"
       ref={ref}
       size="invisible"
-      sitekey="6LcwYyQkAAAAAMsq2VnRYkkqNqLt-ljuy-gfmPYn"
+      sitekey={SITE_KEY}
     />
   );
 });
