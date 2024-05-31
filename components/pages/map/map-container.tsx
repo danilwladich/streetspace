0f6bee3a52c91d55e9a268e2c72290d43ca484b2
Daniel <118133808+danilwladich@users.noreply.github.com>
2024-05-31 18:09:41 +0200

    Remove map status component

--- components/pages/map/map-container.tsx
+++ components/pages/map/map-container.tsx
@@ -4,7 +4,6 @@ import { useMapStore } from "@/hooks/store/use-map-store";
 
 import MapMainContainer from "@/components/common/map-container/map-main-container";
 import Markers from "./markers";
-import MapStatus from "./map-status";
 
 export default function MapContainer() {
   const { position, zoom } = useMapStore();
@@ -12,7 +11,6 @@ export default function MapContainer() {
   return (
     <MapMainContainer position={position || [52.243427, 21.001797]} zoom={zoom}>
       <Markers />
-      <MapStatus />
     </MapMainContainer>
   );
 }
