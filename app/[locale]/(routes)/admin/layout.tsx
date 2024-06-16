837e4c4a427928fb6a1afa4044369e497c0eda3d
Daniel <118133808+danilwladich@users.noreply.github.com>
2024-06-16 13:11:02 +0200

    Update not-found page (add go back button)

--- app/[locale]/(routes)/admin/layout.tsx
+++ app/[locale]/(routes)/admin/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  robots: "noindex,nofollow",
+  robots: {
+    index: false,
+    follow: false,
+  },
 };
 
 export default function AdminLayout({
--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,15 +1,35 @@
+"use client";
+
+import { useRouter } from "@/lib/navigation";
 import { useTranslations } from "next-intl";
+import type { Metadata } from "next";
 
 import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
 
 export default function NotFound() {
-  const t = useTranslations();
+  const t = useTranslations("pages.notFound");
+  const router = useRouter();
 
   return (
     <div className="fixed left-0 top-0 flex h-screen w-screen items-center justify-center gap-6 bg-background">
       <span className="text-2xl font-semibold">404</span>
-      <Separator orientation="vertical" className="h-14" />
-      <h3 className="text-lg">{t("pages.notFound")}</h3>
+      <Separator orientation="vertical" className="h-16" />
+
+      <div className="flex flex-col items-center justify-center gap-1">
+        <h3 className="text-lg">{t("title")}</h3>
+
+        <Button variant="link" size="sm" onClick={router.back}>
+          {t("back")}
+        </Button>
+      </div>
     </div>
   );
 }
