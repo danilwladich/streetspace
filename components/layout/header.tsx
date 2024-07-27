0edb5e7ae080f0565faf751d6ac8cfeabde1cb68
Daniel <118133808+danilwladich@users.noreply.github.com>
2024-07-27 10:50:40 +0200

    Update header component

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useMediaQuery } from "@/hooks/use-media-query";
+import { Link } from "@/lib/navigation";
 import { getAppTitle } from "@/lib/get-app-title";
 
 import { Navigation } from "@/components/layout/navigation/navigation";
@@ -17,7 +18,9 @@ export function Header() {
     <header className="fixed left-0 top-0 z-40 w-full overflow-hidden bg-[rgba(255,255,255,0.75)] backdrop-blur-sm dark:bg-[rgba(0,0,0,0.75)]">
       <div className="pl-[calc(100vw-100%)]">
         <div className="container flex items-center justify-between">
-          <span className="truncate text-lg">{getAppTitle()}</span>
+          <Link href="/" className="truncate text-lg">
+            {getAppTitle()}
+          </Link>
 
           <Navigation />
         </div>
